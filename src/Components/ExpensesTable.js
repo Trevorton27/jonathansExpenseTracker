d175bb65e2f30f445fcf4921d39591d8f790953b
Trevor Mearns <Trevor Mearns>
2020-11-17 14:27:34 +0900

    fixed expense item id render issue

--- src/Components/ExpensesTable.js
+++ src/Components/ExpensesTable.js
@@ -2,19 +2,48 @@ import React from 'react';
 import { Table, Button } from 'react-bootstrap';
 
 const ExpensesTable = (props) => {
-  const expensesArray = [];
+  console.log('expense table props: ', props);
+  //   const expensesArray = [];
 
-  for (let i = 0; i < props.expenses.length; i++) {
-    const expense = props.expenses[i];
-    const id =
-      expensesArray.length > 0
-        ? expensesArray[expensesArray.length - 1].index + 1
-        : 1;
-    expensesArray.push(
-      <tr key={expense.id}>
-        <td>{id}</td>
+  //   for (let i = 0; i < props.expenses.length; i++) {
+  //     const expense = props.expenses[i];
+  //     const id =
+  //       expensesArray.length > 0
+  //         ? expensesArray[expensesArray.length - 1].index + 1
+  //         : 1;
+  //     expensesArray.push(
+  //       <tr key={expense.id}>
+  //         <td>{id}</td>
+  //         <td>{expense.description}</td>
+  //         <td>{expense.amount}</td>
+  //         <td>{expense.payment}</td>
+  //         <td>{expense.date}</td>
+  //         <td>
+  //           <Button
+  //             className='btn btn-danger'
+  //             onClick={(e) => props.deleteExpense(expense.id)}
+  //           >
+  //             Delete
+  //           </Button>
+  //         </td>
+  //       </tr>
+  //     );
+  //   }
+
+  const expenses = props.expenses;
+  const expense = expenses.map((expense, num) => {
+    return (
+      <tr
+        className='strikethrough'
+        data-toggle='tooltip'
+        data-placement='top'
+        title='Want to delete? Just click!'
+        onClick={() => props.deleteExpense(expense.id)}
+        key={num}
+      >
+        <td>{num + 1}</td>
         <td>{expense.description}</td>
-        <td>{expense.amount}</td>
+        <td>${expense.amount}</td>
         <td>{expense.payment}</td>
         <td>{expense.date}</td>
         <td>
@@ -27,9 +56,9 @@ const ExpensesTable = (props) => {
         </td>
       </tr>
     );
-  }
+  });
 
-  console.log('expensesArray: ', expensesArray);
+  console.log('expensesArray: ', expense);
   return (
     <Table
       className='main-table w-75 mx-auto mt-5 text-center'
@@ -48,7 +77,7 @@ const ExpensesTable = (props) => {
         </tr>
       </thead>
 
-      <tbody>{expensesArray}</tbody>
+      <tbody>{expense}</tbody>
     </Table>
   );
 };
